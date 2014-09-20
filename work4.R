b9d7a5268cc9c655a6e8e12826aa48e5ed394c80
JBed <Jason.Bedford@yale.edu>
2014-09-20 19:47:20 -0400

    added cross validation with k=6
    my computer can not do more that k=6 :(

--- work4.R
+++ work4.R
@@ -14,24 +14,47 @@ loans$loan_status <- as.factor(loans$loan_status)
 loans$purpose <- as.factor(loans$purpose)
 is.factor(loans$is_inc_v) #stringsAsFactor worked
 
+summary(loans$last_fico_range_high)
+
 #PART1 try to perdic who pays
 
 # take a look at what we are trying to perdict
 summary(loans$loan_status)
 
 # build logistic regression model using the glm
-fit <- glm(loan_status ~  loan_amnt + term + int_rate + installment + home_ownership + annual_inc + is_inc_v + purpose , data=loans, family=binomial(link=logit))
+fit <- glm(loan_status ~  loan_amnt + term + last_fico_range_high + installment + home_ownership + annual_inc + is_inc_v, data=loans, family=binomial(link=logit))
 summary(fit) # show results
+
+anova(fit)
+
+vcov(fit)
+
+layout(1) 
 plot(fit)
+
 
 #now let's compute some bic
 library("BMA")
 
-output <- bic.glm(loan_status ~  loan_amnt + term + int_rate + installment + annual_inc + is_inc_v , data=loans, glm.family="binomial")
-                      
-warnings()
+output <- bic.glm(loan_status ~  loan_amnt + term + last_fico_range_high + installment + home_ownership + annual_inc + is_inc_v, data=loans, glm.family="binomial")
+
 summary(output)
 imageplot.bma(output)
+
+output$postprob
+output$mle
+output$label
+
+###cross validating
+library("boot")
+
+outcv <- cv.glm(loans, fit)
+
+outcv.6 <- cv.glm(loans, fit,K=6)
+
+summary(outcv.6)
+
+plot(outcv.6)
 
 #nwo let's say that we have a new model
 
@@ -46,6 +69,8 @@ imageplot.bma(output)
 
 
 #and were good. 
+
+
 
 
 

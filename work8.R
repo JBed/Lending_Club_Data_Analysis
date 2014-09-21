41b327cada5b1d2bfddaa3a1b2c1036ef24ef668
JBed <Jason.Bedford@yale.edu>
2014-09-21 19:22:10 -0400

    added work from 9/22

---
+++ work8.R
@@ -0,0 +1,64 @@
+
+#load the data 
+setwd("~/1.WebSite/8.DataSci/LendingClub/2014-Now")
+loans <- read.csv("small.csv", header=TRUE, stringsAsFactors=TRUE, skip=1)
+
+#make the factors
+loans$term <- as.factor(loans$term)
+loans$int_rate <- as.factor(loans$int_rate)
+loans$grade <- as.factor(loans$grade)
+loans$sub_grade <- as.factor(loans$sub_grade)
+loans$home_ownership <- as.factor(loans$home_ownership)
+loans$loan_status <- as.factor(loans$loan_status)
+loans$purpose <- as.factor(loans$purpose)
+is.factor(loans$is_inc_v) #stringsAsFactor worked
+loans$sub_grade <- as.factor(loans$sub_grade)
+loans$grade <- as.factor(loans$grade)
+
+summary(loans$last_fico_range_high)
+summary(loans$loan_status)
+summary(loans$current)
+summary(loans$term)
+
+
+vector <-1
+for (i in seq(loans$loan_status)) {
+  if (loans$loan_status[i] == "Current") {
+    vector[i]<- 1
+  }
+  else{
+    vector[i]<- 0
+  }
+}
+
+
+summary(as.factor(vector))
+loans$current <- as.factor(vector)
+
+
+
+
+
+#let's random forrest
+library(randomForest)
+library(ROCR)
+
+#train it
+rf1 <- randomForest(current ~  loan_amnt + term + last_fico_range_high + installment + home_ownership + annual_inc + is_inc_v, loans, keep.forest=TRUE,importance=TRUE)
+
+
+
+importance(rf1)
+varImpPlot(rf1)
+
+
+
+
+rf2 <- randomForest(current ~  loan_amnt + term + last_fico_range_high + installment + home_ownership + annual_inc + is_inc_v + int_rate + as.factor(loans$sub_grade) +  as.factor(loans$grade), loans, keep.forest=TRUE,importance=TRUE)
+
+importance(rf2)
+varImpPlot(rf2)
+
+
+#i'm ready to start writing. 
+
